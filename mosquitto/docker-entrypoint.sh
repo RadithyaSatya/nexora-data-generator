618b79a827e5a098a0b9029c1514df6a0f51aef3
Radithyasatya <radithyasatya7@gmail.com>
2026-05-03 20:16:40 +0700

    build: fix issue build

--- mosquitto/docker-entrypoint.sh
+++ mosquitto/docker-entrypoint.sh
@@ -10,5 +10,7 @@ PASSWD_FILE="/mosquitto/config/passwd"
 
 rm -f "$PASSWD_FILE"
 mosquitto_passwd -b -c "$PASSWD_FILE" "$MQTT_USERNAME" "$MQTT_PASSWORD"
+chown mosquitto:mosquitto "$PASSWD_FILE"
+chmod 640 "$PASSWD_FILE"
 
 exec /docker-entrypoint.sh "$@"
